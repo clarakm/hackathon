92f922dcf4d3bee96f6a040993f5f865edeb5be4
Clara <58710610+clarakm@users.noreply.github.com>
2020-01-30 12:23:39 -0800

    day 2

--- background.js
+++ background.js
@@ -7,33 +7,65 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 }, false)
 
+function onsub() {
+    chrome.windows.create({url: "redirect.html", type:"popup"});
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('submit').addEventListener('click',
-    onsub, false)
+    countdown, false)
     //e.preventDefault();
-    function onsub() {
-        let num = document.getElementById('time').value;
-    console.log(typeof num);
-    //alert('hi')
-    let newNum = parseInt(num) * 1000;
-    console.log(typeof newNum)
-    setTimeout(function() {alert ('bye')}, newNum);
-    }
+    // function onsub() {
+    //     chrome.windows.create({url: "redirect.html", type:"popup"});
+    //     let num = document.getElementById('time').value;
+    // console.log(typeof num);
+    // 
+    // let newNum = parseInt(num) * 1000;
+    // console.log(typeof newNum)
+    // setTimeout(function() {alert ('bye')}, newNum);
+    //}
 }, false)
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     document.getElementById('breathe').addEventListener('click',
-//     startBreathe, false)
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('inhale').addEventListener('click',
+    countdown, false)
     
-//     function startBreathe() {
-//         let changeColor= document.getElementsByTagName('body');
-//         for (let b of changeColor) {
-//             b.style['background-color'] = 'black'
-//         }
+    // function startBreathe() {
+    //     let changeColor= document.getElementById('inhale');
+    //     changeColor.innerHTML = "EXHALE";
+    //     console.log("test")
+    //     let change2 = document.getElementById
        
        
-//     }
-// }, false)
+    // }
+    
+}, false)
+
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.windows.create({'url': 'redirect.html','type': 'popup'}, function(window) {
+        console.log('hello');
+    });
+});
+
+// let remind = document.getElementById('submit');
+// remind.addEventListener('click', function () {
+//     alert('it me!')
+// });
+
+//countdown
+function countdown() {
+    let timeleft = 10;
+    let downloadTimer = setInterval(function(){
+    document.getElementById("countdown").innerHTML = "00 : " + timeleft;
+    timeleft -= 1;
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        onsub();
+        //document.getElementById("countdown").innerHTML = "Finished"
+    }
+    }, 1000);
+}
+
 
 
 
@@ -48,14 +80,4 @@ function myfunc() {
     
 }
 
-
-
-
-
-
-// let time = setInterval(myTimer, 1000);
-// function myTimer() {
-//     let d = new Date();
-//     document.getElementById("counter").innerHTML = d.toLocaleTimeString();
-// }
 
