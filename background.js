4e4179d4945ee52849a913247c491ba07b8a1fd2
Clara <58710610+clarakm@users.noreply.github.com>
2020-01-30 17:32:28 -0800

    bloom complete!!

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('title').addEventListener('click',
     onclick, false)
-    //e.preventDefault();
     function onclick() {
         alert('Take a breather!')
     }
@@ -11,54 +10,42 @@ function onsub() {
     chrome.windows.create({url: "redirect.html", type:"popup"});
 }
 
+function onStretch() {
+    chrome.windows.create({url: "stretch.html", type:"popup"});
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('submit').addEventListener('click',
     countdown, false)
-    //e.preventDefault();
-    // function onsub() {
-    //     chrome.windows.create({url: "redirect.html", type:"popup"});
-    //     let num = document.getElementById('time').value;
-    // console.log(typeof num);
-    // 
-    // let newNum = parseInt(num) * 1000;
-    // console.log(typeof newNum)
-    // setTimeout(function() {alert ('bye')}, newNum);
-    //}
 }, false)
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('inhale').addEventListener('click',
-    countdown, false)
-    
-    // function startBreathe() {
-    //     let changeColor= document.getElementById('inhale');
-    //     changeColor.innerHTML = "EXHALE";
-    //     console.log("test")
-    //     let change2 = document.getElementById
-       
-       
-    // }
-    
+    onsub, false)   
 }, false)
 
-chrome.browserAction.onClicked.addListener(function() {
-    chrome.windows.create({'url': 'redirect.html','type': 'popup'}, function(window) {
-        console.log('hello');
-    });
-});
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('stretch').addEventListener('click',
+    onStretch, false)
+}, false)
 
-// let remind = document.getElementById('submit');
-// remind.addEventListener('click', function () {
-//     alert('it me!')
-// });
 
-//countdown
+
 function countdown() {
-    let timeleft = 10;
+    // converts to secon
+    let num = document.getElementById('time').value;
+    let newNum = parseInt(num);
+    let timeleft = newNum - 1;
+    let secondsLeft = 59;
     let downloadTimer = setInterval(function(){
-    document.getElementById("countdown").innerHTML = "00 : " + timeleft;
-    timeleft -= 1;
-    if(timeleft <= 0){
+        // if(secondsLeft < 60)
+    document.getElementById("countdown").innerHTML =  timeleft + ": " + secondsLeft;
+    if(secondsLeft === 0) {
+        timeleft -= 1;
+        secondsLeft = 59;
+    }
+    secondsLeft -= 1;
+    if(timeleft < 0){
         clearInterval(downloadTimer);
         onsub();
         //document.getElementById("countdown").innerHTML = "Finished"
@@ -67,17 +54,5 @@ function countdown() {
 }
 
 
-
-
-function myfunc() { 
-    //console.log(document.getElementById('time').value);
-    let num = document.getElementById('time').value;
-    console.log(typeof num);
-    //alert('hi')
-    let newNum = parseInt(num) * 1000;
-    console.log(typeof newNum)
-    setTimeout(function() {alert ('bye')}, newNum);
-    
-}
 
 
--- content.js
+++ content.js
@@ -1,26 +1,15 @@
 console.log('chrome ext');
 
 //highlights all p tags
-const paragraphs = document.getElementsByTagName('p');
-for (let p of paragraphs) {
-    p.style['background-color'] = '#d6fff1'
-}
+// const paragraphs = document.getElementsByTagName('p');
+// for (let p of paragraphs) {
+//     p.style['background-color'] = '#d6fff1'
+// }
 
 
 
-// let changeColor= document.getElementsByTagName('div');
-        
-//     for (let b of changeColor) {
-//             b.style['background-color'] = 'black'
-//         }
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('inhale').addEventListener('click', inhaleExhale, false)
-    function inhaleExhale() {
-        document.getElementById('inhale').innerHTML = "Exhale!";
-        console.log('inahle')
-    }
 
-}, false)
+
 
 
 
